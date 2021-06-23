ce09dc0e954f5fa05289a0401cade47b1d58948c
jp-hidalgo <jp.hidalgo@uniandes.edu.co>
2021-06-23 08:16:31 -0500

    MongoDB attlas connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://jphidalgo:rbQE0d4NIcble6N7@cluster0.4ufpq.mongodb.net/fifa?retryWrites=true&w=majority"
+  url: "mongodb+srv://jphidalgo:"+process.env.DBPASSWORD+"@cluster0.4ufpq.mongodb.net/fifa?retryWrites=true&w=majority"
 };
