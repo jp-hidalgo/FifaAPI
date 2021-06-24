9da500d9a3b9b36f3414c6517af5fa0a8b9b660a
jp-hidalgo <jp.hidalgo@uniandes.edu.co>
2021-06-23 20:32:51 -0500

    docker

--- app/controllers/player.controller.js
+++ app/controllers/player.controller.js
@@ -7,10 +7,10 @@ exports.create = (req, res) => {
     res.status(400).send({ message: "Content can not be empty!" });
     return;
   }
-  if(!req.header("authkey")||req.header("authkey")!==process.env.API_KEY){
+  /*if(!req.header("authkey")||req.header("authkey")!==process.env.API_KEY){
     res.status(401).send({message: "Unauthorized"});
     return;
-  }
+  }*/
   // Create a Player
   const player = new Player({
     name: req.body.name,
@@ -32,10 +32,31 @@ exports.create = (req, res) => {
       });
     });
 };
-
-// Retrieve all Players from the database.
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  /*if(!req.header("authkey")||req.header("authkey")!==process.env.API_KEY){
+    res.status(401).send({message: "Unauthorized"});
+    return;
+  }*/
+  Player.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Player with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Player with id=" + id });
+    });
+};
+// Retrieve all Players from the database./find by condition
 exports.findTeam = (req, res) => {
   const team = req.query.team;
+  /*if(!req.header("authkey")||req.header("authkey")!==process.env.API_KEY){
+    res.status(401).send({message: "Unauthorized"});
+    return;
+  }*/
   var condition = team ? { team: { $regex: new RegExp(team), $options: "i" } } : {};
 
   Player.find(condition)
--- app/routes/player.routes.js
+++ app/routes/player.routes.js
@@ -1,16 +1,19 @@
 module.exports = app => {
-    const players = require("../controllers/player.controller.js");
+  const players = require("../controllers/player.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Player
+  router.post("/", players.create);
+
+  // Retrieve all Players
+  router.post("/team", players.findTeam);
+
+  // Retrieve Players with name
+  router.get("/players", players.findPlayers);
   
-    var router = require("express").Router();
-  
-    // Create a new Player
-    router.post("/", players.create);
-  
-    // Retrieve all Players
-    router.post("/team", players.findTeam);
-  
-    // Retrieve Players with name
-    router.get("/players", players.findPlayers);
-  
-    app.use('/api/v1', router);
-  };
+  // Retrieve a single Player with id
+  router.get("/:id", players.findOne);
+
+  app.use('/api/v1', router);
+};
